e3baf1f667258b4eca7eaa369805c235015c038b
Sean Langton <slangton1@gmail.com>
2018-11-19 19:12:26 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,9 +1,11 @@
 module Players
 class Computer < Player
 
+CORNER_INPUTS = [1,3,7,9]
+MIDDLE_INPTUS = [2,4,6,8]
   def move(board)
     if board.position("5") == " " then "5"
-    else [1,3,7,9].sample.to_i
+    elsif [1,3,7,9].delete_if {|cell| board.position(cell).taken?}
       end
   end
 
